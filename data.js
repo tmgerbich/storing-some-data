fffbe9b264f95467e0ce2978bf83dc2734d29bee
tmgerbich <therese.m.gerbich@gmail.com>
2024-06-11 08:34:37 -0400

    added arrays

---
+++ data.js
@@ -0,0 +1,8 @@
+
+let names = ['Therese', 'Alex', 'Gregg', 'Daylen', 'Drew'];
+let food = ['chicken', 'steak', 'pasta', 'hamburger', 'pizza', 'salad', 'burrito', 'taco', 'cookie', 'pie'];
+let states = [['Alabama', 'Montgomery', 'Yellowhammer'], ['Alaska', 'Juneau', 'Willow Ptarmington'], ['Arizona', 'Phoenix', 'Cactus Wren'], ['Arkansas', 'Little Rock', "Northern Mockingbird"], ['California', 'Sacramento', 'California Quail'], ['Colorado', 'Denver', 'Lark Bunting'], ['Connecticut', 'Hartford', 'American Robin']];
+
+console.log(names);
+console.log(food);
+console.log(states);
